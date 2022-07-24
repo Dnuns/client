798cc4858533f802f567513e94905f1f0a0e1f7c
David Nunes <51808846+Dnuns@users.noreply.github.com>
2022-07-24 09:32:13 -0100

    feat: get by Id

--- src/main/java/com/example/client/controller/ClientController.java
+++ src/main/java/com/example/client/controller/ClientController.java
@@ -1,13 +1,12 @@
 package com.example.client.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,13 +15,13 @@ import com.example.client.dto.ClientDTO;
 import com.example.client.service.ClientService;
 
 @RestController
-@RequestMapping(value = "/v1")
+@RequestMapping(value = "/clients")
 public class ClientController {
 
 	@Autowired
 	private ClientService service;
 	
-	@GetMapping(value = "/clients")
+	@GetMapping
 	public ResponseEntity<Page<ClientDTO>> findAll(
 			@RequestParam(value = "page", defaultValue ="0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue ="5") Integer linesPerPage,
@@ -34,5 +33,10 @@ public class ClientController {
 		
 		Page<ClientDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+		return ResponseEntity.ok().body(service.findById(id));
 	}
 }
